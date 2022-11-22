70c6c82a4204789e0d79b66eb45950b27d172502
Josh Myatt <josh@joshs-air-4.wireless-1x.unc.edu>
2022-11-22 17:26:38 -0500

    added xcode project

---
+++ UNCGuesser/UNCGuesser/UNCGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNCGuesserApp.swift
+//  UNCGuesser
+//
+//  Created by Josh Myatt on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct UNCGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
