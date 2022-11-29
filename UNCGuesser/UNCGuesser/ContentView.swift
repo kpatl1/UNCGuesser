7f5dd6163485f4985ae8308e5fd9c1924f1a8b63
Kishan Patel <kpatel6@unc.edu>
2022-11-29 03:32:40 -0500

    added round and guess counter. minimized repeat locations. need to add game over screen.

--- UNCGuesser/UNCGuesser/ContentView.swift
+++ UNCGuesser/UNCGuesser/ContentView.swift
@@ -15,31 +15,38 @@ struct ContentView: View {
     @State private var locationsDict = ["Abernethy Annex": [35.897587, -79.057671] ,"Art Studio Building":[35.931885, -79.058293],"Arts and Sciences Foundation": [35.918081, -79.046419], "Friday Center": [35.897316, -79.012161],"FedEx Global Education Center": [35.907817, -79.054528],"Davis Library":[35.910959, -79.047907],"Health Sciences Library":[35.908366, -79.052776], "House Undergraduate Library": [35.909768, -79.049045]]
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.9, longitude: -79.05), span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+    @State private var shownLocations = [String]()
     @State var mapLocations = [Location]()
     @State var score = [Int]()
     @State var scoreCount = 0
-
+    @State var roundNumber = 1
+    @State var numGuesses = 0
+    @State var shouldHide = false
+    @State var locationPlaced = false
+    
+    
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
-
+            .ignoresSafeArea()
+            
             VStack {
                 Spacer()
-
+                
                 Text("Guess the Location")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
-
+                
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the location of")
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
-
+                        
                         Text(locations[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
@@ -59,6 +66,7 @@ struct ContentView: View {
                                 Button {
                                     let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: region.center.latitude, longitude: region.center.longitude)
                                     addLocation(newLocation: newLocation)
+                                    self.locationPlaced = true
                                 } label: {
                                     Image(systemName: "plus")
                                 }
@@ -71,22 +79,42 @@ struct ContentView: View {
                             }
                         }
                     }
-                    Button(action: {verify(scoreCount: scoreCount, score: score)}) {
+                    
+                    if !self.shouldHide{
+                        Button(action: {verify(scoreCount: scoreCount, score: score); if locationPlaced {numGuesses += 1};}){
                             Text("Confirm")
-                            .padding()
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                            .background(Color.blue)
-                            .cornerRadius(30)
+                                .padding()
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(30)
+                        }
                     }
+                    else{
+                        Button(action: {self.shouldHide = false; }){
+                            Text("New Round")
+                                .padding()
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                                .background(Color.blue)
+                                .cornerRadius(30)
+                        }
+                    }
+                    
+                    
+                    Text("Round Number: \(roundNumber) ")
+                        .bold()
+                    Text("Guesses: \(numGuesses) ")
+                    
                     
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
                 .background(.regularMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
-
+                
             }
+            
             .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
@@ -103,28 +131,73 @@ struct ContentView: View {
         }
         mapLocations.append(newLocation)
     }
-
+    
     func locationTapped() {
-        scoreTitle = String(score[scoreCount-1])
-        showingScore = true
+        
+        if numGuesses >= 2{
+            showingScore = false
+        }
+        else{
+            scoreTitle = String(score[scoreCount-1])
+            showingScore = true
+        }
+        
     }
-
+    
     func askQuestion() {
         locations.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        if locations.count != shownLocations.count{
+            let tempAnswer = Int.random(in: 0...2)
+            if shownLocations.contains(locations[tempAnswer]){
+                askQuestion()
+            }
+            correctAnswer = tempAnswer
+        }
+        else{
+            locations.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
     }
+    
+    
+    func newRound(){
+        self.shouldHide = true
+        self.scoreCount = 0
+        self.numGuesses = -1
+        self.roundNumber += 1
+        self.score.removeAll()
+        self.shownLocations.removeAll()
+        
+    }
+    
+    func gameOver(){
+        self.shownLocations.removeAll()
+        self.roundNumber = 0
+        self.scoreCount = 0
+        self.numGuesses = -1
+        self.score.removeAll()
+    }
+    
     func verify(scoreCount: Int, score: [Int]){
         //make sure locations array has a value
         if mapLocations.count != 0 {
             self.scoreCount+=1
             print(locationsDict[locations[correctAnswer]]![0])
             print(locationsDict[locations[correctAnswer]]![1])
-
-            var lat = (1000*(abs(mapLocations[0].latitude - locationsDict[locations[correctAnswer]]![0])))
-            var long = (1000*(abs(mapLocations[0].longitude - locationsDict[locations[correctAnswer]]![1])))
+            
+            let lat = (1000*(abs(mapLocations[0].latitude - locationsDict[locations[correctAnswer]]![0])))
+            let long = (1000*(abs(mapLocations[0].longitude - locationsDict[locations[correctAnswer]]![1])))
             self.score.append(Int(100-(lat+long)/2))
-            locationTapped()
-
+            if numGuesses >= 2 && roundNumber <= 4{
+                newRound()
+            }
+            else if roundNumber >= 5{
+                gameOver()
+            }
+            else{
+                locationTapped()
+            }
+            
         }
     }
 }
@@ -134,3 +207,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
